8ada144ad4c705c6eaf8252e3377aa76690e4a6a
Tobi Adegbuji <adegbuji_tobi@yahoo.com>
2021-07-28 07:01:13 -0400

    Initial commit

---
+++ src/test/java/dev/tobiadegbuji/jmstutorial/JmsTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tobiadegbuji.jmstutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
